816d2e96ced3eee4c2096b7f879632402f131617
Rodrigo Silva <aniqdevcorp@gmail.com>
2025-04-16 16:19:43 -0300

    [ANQ-001] feat: added custom error messages

--- src/users/application/request/CreateUserRequest.ts
+++ src/users/application/request/CreateUserRequest.ts
@@ -1,16 +1,16 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateUserRequest {
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: "name should not be empty" })
+  @IsString({ message:'name should be a valid JSON format string' })
   public name: string
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: "username should not be empty" })
+  @IsString({ message:'username should be a valid JSON format string' })
   public username: string
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: "password should not be empty" })
+  @IsString({ message:'password should be a valid JSON format string' })
   public password: string
-  @IsNotEmpty()
-  @IsNumber()
+  @IsNotEmpty({ message: "accountBalance should not be empty" })
+  @IsNumber({}, { message: "accountBalance should be a valid JSON number, representing you initial account ballance like 233.50 or just 233" })
   public accountBalance: number
 }
