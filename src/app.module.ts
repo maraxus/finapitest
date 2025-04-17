52aec3f9074b9da89d1fc818c9fbaf06506815ee
Rodrigo Silva <aniqdevcorp@gmail.com>
2025-04-17 15:13:12 -0300

    [ANQ-001] fix: removed redundandt service and controller declaration to solve a DI bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,8 @@ import { AuthenticationModule } from './authentication/authentication.module';
 import { WalletModule } from './wallet/wallet.module';
 import { CommonModule } from './common/common.module';
 import { CoreModule } from './core/core.module';
-import { UsersController } from './users/application/users.controller';
-import { CreateUser } from './users/domain/useCase/createUser';
 
 @Module({
-  imports: [UsersModule, AuthenticationModule, WalletModule, CommonModule, CoreModule],
-  controllers: [UsersController],
-  providers: [CreateUser],
+  imports: [UsersModule, AuthenticationModule, WalletModule, CommonModule, CoreModule]
 })
 export class AppModule {}
