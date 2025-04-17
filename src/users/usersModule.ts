a299d514dcab10e0e3184583a09d4824c938c97e
Rodrigo Silva <aniqdevcorp@gmail.com>
2025-04-17 15:15:12 -0300

    [ANQ-001] fix: added account repository provider

--- src/users/usersModule.ts
+++ src/users/usersModule.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './application/users.controller';
 import { CreateUser } from './domain/useCase/createUser';
+import { AccountRepository } from './domain/Repository/AccountRepository';
 
 @Module({
   controllers: [UsersController],
-  providers: [CreateUser]
+  providers: [
+    {
+      provide: "AccountRepositoryInterface",
+      useClass: AccountRepository
+    },
+    CreateUser
+  ]
 })
 export class UsersModule {}
