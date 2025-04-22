1701b8b6cacb04c66f4ac6a7da6ddb7f9606623f
Rodrigo Silva <aniqdevcorp@gmail.com>
2025-04-22 12:28:43 -0300

    [ANQ-001] feat: configured typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,22 @@ import { AuthenticationModule } from './authentication/authentication.module';
 import { WalletModule } from './wallet/wallet.module';
 import { CommonModule } from './common/common.module';
 import { CoreModule } from './core/core.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'process';
+import { UserDAO, WalletDAO } from './users/domain/Repository/AccountRepository';
 
 @Module({
-  imports: [UsersModule, AuthenticationModule, WalletModule, CommonModule, CoreModule]
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'wallet',
+      password: process.env.PGDBPASS,
+      database: 'walletdb',
+      entities: [UserDAO, WalletDAO],
+      synchronize: false
+    }),
+    UsersModule, AuthenticationModule, WalletModule, CommonModule, CoreModule]
 })
 export class AppModule {}
