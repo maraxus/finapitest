96c3dc8c772ed41b5a454922dd7699c477223346
Rodrigo Silva <aniqdevcorp@gmail.com>
2025-04-22 12:33:27 -0300

    [ANQ-001] feat: implemented static constructor to workaround single constructors in typescript and implement entity hidration

--- src/core/valueObjects/UuidID.ts
+++ src/core/valueObjects/UuidID.ts
@@ -1,12 +1,28 @@
-import { v7 as uuidv7 } from 'uuid';
+import { v7 as uuidv7, validate as uuidValidate} from 'uuid';
+
+export const INVALID_UUID_ERROR = "invalid_uuid"
+class InvalidUUID implements Error {
+  constructor() {}
+  message: string = 'tried to create a uuid from a invalid uuid string';
+  name: "INVALID_UUID";
+}
 
 export class UuidID {
   private readonly id
-  constructor() {
-    this.id = uuidv7({ msecs: Date.now() })
+  private constructor(id: string) {
+    this.id = id
   }
   toString() {
     return this.id.toString()
+  }
+
+  public static fromString(id: string) {
+    if(!uuidValidate(id)) throw new InvalidUUID()
+    return new UuidID(id)
+  }
+
+  public static generate(): UuidID {
+    return new UuidID(uuidv7({ msecs: Date.now() }))
   }
 }
 
