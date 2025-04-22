34f90975380e05d4b623b054b803ba961fb2d83f
Rodrigo Silva <aniqdevcorp@gmail.com>
2025-04-22 12:31:18 -0300

    [ANQ-001] feat: creating Enities using new static creators instead of the constructor, directly

--- src/users/domain/useCase/createUser.ts
+++ src/users/domain/useCase/createUser.ts
@@ -3,7 +3,7 @@ import { Money } from '../../../core/valueObjects/money';
 import { UserUid } from '../../../core/valueObjects/UuidID';
 import { User } from '../Entity/User';
 import { Wallet } from '../Entity/Wallet';
-import { AccountRepositoryInterface, AccountRepositoryToken } from '../Repository/AccountRepositoryInterface';
+import { AccountRepositoryInterface } from '../Repository/AccountRepositoryInterface';
 
 export type CreatedUser = {
   account: UserUid,
@@ -18,8 +18,8 @@ export type CreatedUser = {
 export class CreateUser {
   constructor(@Inject("AccountRepositoryInterface") private accountRepository: AccountRepositoryInterface) {}
   async execute(name: string, username: string, password: string, initialBalance: number): Promise<CreatedUser> {
-    const user = new User(name, username, password)
-    const wallet = new Wallet(user, new Money(initialBalance))
+    const user = User.create(name, username, password)
+    const wallet = Wallet.create(user, new Money(initialBalance))
     const saved = await this.accountRepository.register(user, wallet)
     return this.toOutput(saved);
   }
